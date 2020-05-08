78d04dee16377e71efbf56e102f313ade243c060
Migs <mmejares@hotmail.com>
2020-05-08 00:31:27 -0500

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let User;
+
+if (!User) {
+    let userSchema = new Schema({
+        email: {type: String, require: true, lowercase: true, unique: true},
+        password: {type: String, required: true}
+    },
+    {
+        timestamps: true
+    });
+    
+    User = mongoose.model('User, userSchema');
+}
+
+
+
+module.exports = User;
